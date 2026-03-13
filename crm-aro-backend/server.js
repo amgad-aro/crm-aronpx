e930072fcbd1f96d4754d30d7be96dab100ed4f5
Amgad <amgadm22@gmail.com>
2026-03-13 03:04:15 +0200

    fix NewLead status

--- crm-aro-backend/server.js
+++ crm-aro-backend/server.js
@@ -377,6 +377,7 @@ app.post("/api/daily-requests", auth, async function(req, res) {
       agentId: req.body.agentId || req.user.id,
       callbackTime: req.body.callbackTime || "",
       lastActivityTime: new Date(),
+status: req.body.status || "NewLead",
     });
     r = await DailyRequest.findById(r._id).populate("agentId","name title");
     res.json(r);
