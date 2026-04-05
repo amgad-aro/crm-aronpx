99cc727e498df09af59b92b6bf3d741280ddd0b1
Amgad <amgadm22@gmail.com>
2026-04-05 23:04:00 +0200

    Restrict backend CORS to approved origins only

--- crm-aronpx-main/crm-aro-backend/server.js
+++ crm-aronpx-main/crm-aro-backend/server.js
@@ -9,7 +9,22 @@ var jwt = require("jsonwebtoken");
 var { User, Lead, Activity, Task, DailyRequest } = require("./models.js");
 
 var app = express();
-app.use(cors());
+var allowedOrigins = [
+  "https://crm-aronpx.vercel.app",
+  "http://localhost:3000",
+  "https://script.google.com",
+  "https://script.googleusercontent.com"
+];
+var corsOptions = {
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+};
+app.use(cors(corsOptions));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
